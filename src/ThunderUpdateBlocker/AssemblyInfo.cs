856b188cb65ae824a080c9fba25fcd3bf6e66276
wen.xu <cheer_cheer@aliyun.com>
2021-08-16 16:21:43 +0800

    Initial commit

---
+++ src/ThunderUpdateBlocker/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("迅雷升级程序")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("迅雷升级程序")]
+[assembly: AssemblyCopyright("版权所有 (C) 2020 深圳市迅雷网络技术有限公司")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f83f543d-9317-4629-80fb-2416c75e1eb0")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("3.1.10.89")]
---
+++ src/ThunderUpdateBlocker/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ThunderUpdateBlocker
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // Do nothing here
+        }
+    }
+}
